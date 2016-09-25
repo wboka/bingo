9dd5b8831cdc4914263fece607bb340875ea3978
Wayne Boka <boka.wayne@gmail.com>
2016-09-25 18:47:12 -0400

    - Adds better README - Adds ability to restart a game - Cleaning up HTML markup

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# bingo
+# Bingo
+
+Simple javascript based bingo game.
+
+## Setup
+
+0. Game starts in 5 seconds after page load
+1. Click "Add Bingo Card" as needed
+2. Click numbers on each bingo card as they are called
+3. Click "Bingo!" once you have a valid combination of numbers in a winning pattern
+ * 5 spaces in a row horizontally/vertically
+ * 5 spaces in a diagonal in either direction
+ * The 4 corners
+4. Click "Restart" to play again.
--- bingo.js
+++ bingo.js
@@ -8,7 +8,7 @@ var bingo = {
 		if (typeof numberOfCards == "undefined") {
 			numberOfCards = 1;
 		}
-		
+
 		for (var i = 0; i < numberOfCards; i++) {
 			var bArray = [];
 			var iArray = [];
@@ -17,7 +17,7 @@ var bingo = {
 			var oArray = [];
 			var cardNumbers = {};
 			var numbersAvailable = bingo.shuffle(bingo.availableNumbers);
-			
+
 			for (var j = 0; j < numbersAvailable.length; j++) {
 				var number = numbersAvailable[j].substring(1, numbersAvailable[j].length);
 
@@ -56,7 +56,7 @@ var bingo = {
 						// Something Broke
 				}
 			}
-			
+
 			$("#playing-area").append(bingoCard(cardNumbers));
 		}
 	},
@@ -65,7 +65,7 @@ var bingo = {
 	daub: function() {
 		if (!$(this).hasClass("free")) {
 			$(this).toggleClass("daubed");
-			
+
 			/*
 			if (bingo.calledNumbers.indexOf($(this).data('numberCalled')) === -1) {
 				$(this).removeClass("daubed");
@@ -94,51 +94,51 @@ var bingo = {
 			"BtoODiagonalDown": [],
 			"corners": []
 		};
-		
+
 		$daubed.each(function() {
 			var letter = $(this).data("letter");
 			var row = $(this).data("row");
 			var number = $(this).text();
 			var letterRow = letter + row;
-			var isValidBingo = true; 
-			
+			var isValidBingo = true;
+
 			if (bingo.calledNumbers.indexOf(letter + number) === -1 && (number.toLowerCase() !== "free")) {
 				isValidBingo =  false;
 			}
-			
+
 			if (isValidBingo) {
 				// Column BINGO
 				possibleBingos[letter].push(number);
 				// Row BINGO
 				possibleBingos[row].push(number);
-				
+
 				// Corners
 				if ((/B|O/gi.test(letter)) && (/1|5/gi.test(row))) {
 					possibleBingos.corners.push(number);
 				}
-				
+
 				// BtoODiagonalDown
 				if ((/B1|I2|N3|G4|O5/.test(letterRow))) {
 					possibleBingos.BtoODiagonalDown.push(number);
 				}
-				
+
 				// BtoODiagonalUp
 				if ((/B5|I4|N3|G2|O1/.test(letterRow))) {
 					possibleBingos.BtoODiagonalUp.push(number);
 				}
 			}
 		});
-		
+
 		for (var key in possibleBingos) {
 			if ((key.toLowerCase() === "corners" && possibleBingos[key].length === 4) || possibleBingos[key].length === 5) {
-				$card.find(".call-bingo").after("<div class='alert alert-success text-center'><h1>BINGO!!!</h1>You got BINGO after " + bingo.calledNumbers.length + " balls were called.</div>");
+				$card.find(".call-bingo").after("<div class='alert alert-success text-center bingo'><h1>BINGO!!!</h1>You got BINGO after " + bingo.calledNumbers.length + " balls were called.</div>");
 				$card.find(".call-bingo").remove();
 				$card.find(".daubable").removeClass("daubable");
-				
+
 				return true;
 			}
 		}
-		
+
 		return false;
 	},
 	index: 0,
@@ -146,7 +146,7 @@ var bingo = {
 		while (bingo.availableNumbers.length < 76) {
 			var number = Math.floor(Math.random() * 76) + 1;
 			var letter = "B";
-			
+
 			if (number > 15) { letter = "I"; }
 			if (number > 30) { letter = "N"; }
 			if (number > 45) { letter = "G"; }
@@ -160,60 +160,63 @@ var bingo = {
 	isStarted: false,
 	play: function() {
 		bingo.isStarted = true;
-		
+
 		bingo.initialize();
-		
+
 		timer = setInterval(function() {
 			if (bingo.isStarted && $(".remove-bingo-card").length > 0) {
 				$(".remove-bingo-card").remove();
 			}
-			
+
 			if (bingo.isStarted && $("#add-bingo-card").length > 0) {
 				if ($(".bingo-card").length === 0) {
-					$("#add-bingo-card").click();	
+					$("#add-bingo-card").click();
 				}
-				
+
 				$("#add-bingo-card").remove();
 			}
-			
-			if (bingo.availableNumbers.length === bingo.calledNumbers.length) { 
+
+			if (bingo.availableNumbers.length === bingo.calledNumbers.length || $(".bingo").length === $(".bingo-card").length) {
 				clearInterval(timer);
-				
+
 				alert("Game Over");
-				
+
 				return;
 			}
-			
+
 			$("#bingo-balls").prepend(bingoBall({
-				ballType: bingo.availableNumbers[bingo.index].charAt(0).toLowerCase(), 
+				ballType: bingo.availableNumbers[bingo.index].charAt(0).toLowerCase(),
 				number: bingo.availableNumbers[bingo.index++]
 			}));
-			
+
 			bingo.calledNumbers.push(bingo.availableNumbers[bingo.index - 1]);
 
 			$("#ball-count").text(bingo.availableNumbers.length - bingo.calledNumbers.length);
-			
+
 			$(".call-bingo").click();
 		}, 5000);
 	},
 	removeCard: function() {
 		$(this).closest(".bingo-card").remove();
 	},
+	restart: function() {
+		window.location.reload(true);
+	},
 	shuffle: function(array) {
 		var copiedArray = array.slice();
 		var m = copiedArray.length, t, i;
-		
+
 		// While there remain elements to shuffle…
 		while (m) {
 			// Pick a remaining element…
 			i = Math.floor(Math.random() * m--);
-			
+
 			// And swap it with the current element.
 			t = copiedArray[m];
 			copiedArray[m] = copiedArray[i];
 			copiedArray[i] = t;
 		}
-		
+
 		return copiedArray;
 	}
 };
@@ -221,11 +224,12 @@ var bingo = {
 $(document).ready(function() {
 	bingoCard = Handlebars.compile($("#bingo-card-template").html());
 	bingoBall = Handlebars.compile($("#bingo-ball-template").html());
-	
+
 	$("#add-bingo-card").on("click", bingo.addCard);
 	$("body").on("click", ".remove-bingo-card", bingo.removeCard);
 	$("body").on("click", "td.daubable", bingo.daub);
 	$("body").on("click", ".call-bingo", bingo.hasBingo);
-	
+	$("#restart-bingo").on("click", bingo.restart);
+
 	bingo.play();
 });
